f25aec402272dbc8a38f95cd0f647f96709e23f0
Louie Penaflor <louie.penaflor@getnextt.com>
2013-09-13 15:23:17 -0500

    update

--- lib/siriproxy-jenkins.rb
+++ lib/siriproxy-jenkins.rb
@@ -5,30 +5,29 @@ require 'siri_objects'
 require 'pp'
 
 class SiriProxy::Plugin::Jenkins < SiriProxy::Plugin
+
   def initialize( config = {} )
         @config = config
         @jenkins = JenkinsApi::Client.new( :server_ip =>  @config['jenkins_url'] )
   end
 
   listen_for /status of job (.+)/i do |jobName|
-        #jobs = @jenkins.job.list( jobName, true )
-        if @jenkins.job.exists?( jobName )
-                status = @jenkins.job.get_current_build_status( jobName )
-                say "The current status on job #{jobName} is: #{status}"
-        else
-                say "I couldn't find a job with the name #{jobName}"
-        end
-
-        request_completed
+    if @jenkins.job.exists?( jobName )
+      status = @jenkins.job.get_current_build_status( jobName )
+      say "The current status on job #{jobName} is: #{status}"
+    else
+      say "I couldn't find a job with the name #{jobName}"
+    end
+    request_completed
   end
 
   listen_for /build job (.+)/i do |jobName|
-        if @jenkins.job.exists?( jobName )
-                job_id = @jenkins.job.build( jobName, {}, true )
-                say "Building job #{jobName} build number is #{job_id}"
-        else
-                say "I couldn't find a job with the name #{jobName}"
-        end
-        request_completed
+    if @jenkins.job.exists?( jobName )
+      job_id = @jenkins.job.build( jobName, {}, true )
+      say "Building job #{jobName} build number is #{job_id}"
+    else
+      say "I couldn't find a job with the name #{jobName}"
+    end
+    request_completed
   end
 end
